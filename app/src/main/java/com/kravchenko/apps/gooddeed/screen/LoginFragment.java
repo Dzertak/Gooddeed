3cb50b76e16b91324de70e64ee58870c440bc38c
Ulfhednar <65617581+SaienkoDmytro@users.noreply.github.com>
2021-04-21 23:02:01 +0300

    add google map + get location + binding

--- app/src/main/java/com/kravchenko/apps/gooddeed/screen/LoginFragment.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/screen/LoginFragment.java
@@ -2,15 +2,22 @@ package com.kravchenko.apps.gooddeed.screen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kravchenko.apps.gooddeed.R;
+import com.kravchenko.apps.gooddeed.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,8 +25,23 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+        binding.buttonLoginFragmentLogIn.setOnClickListener(v -> navController.navigate(R.id.action_loginFragment_to_mainFragment));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
--- app/src/main/java/com/kravchenko/apps/gooddeed/screen/MainFragment.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/screen/MainFragment.java
@@ -1,16 +1,39 @@
 package com.kravchenko.apps.gooddeed.screen;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.kravchenko.apps.gooddeed.R;
+import com.kravchenko.apps.gooddeed.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
+
+    private FragmentMainBinding binding;
+    private SupportMapFragment supportMapFragment;
+    private FusedLocationProviderClient fusedLocation;
+    private final static int REQUEST_CODE = 101;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,8 +41,66 @@ public class MainFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+
+
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+        fusedLocation = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        getCurrentLocation();
+
+        return binding.getRoot();
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Task<Location> task = fusedLocation.getLastLocation();
+            task.addOnSuccessListener(location -> {
+                if (location != null) {
+                    supportMapFragment.getMapAsync(googleMap -> {
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        MarkerOptions markerCurrentLocation = new MarkerOptions().position(latLng).title("Im here!");
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                        googleMap.addMarker(markerCurrentLocation);
+
+                    });
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        supportMapFragment.getMapAsync(googleMap -> googleMap.setOnMapClickListener(latLng -> {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title("Marker name here");
+            googleMap.clear();
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+            googleMap.addMarker(markerOptions);
+        }));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
