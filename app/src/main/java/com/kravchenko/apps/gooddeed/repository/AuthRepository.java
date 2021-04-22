33e861732ff1bfc8f487cdedc7398b32ef65b821
a.suprun <alexander.suprun1@gmail.com>
2021-04-22 11:31:10 +0300

    firebase authentication setup; facebook login doesn't work yet

--- app/src/main/java/com/kravchenko/apps/gooddeed/repository/AuthRepository.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/repository/AuthRepository.java
@@ -1,4 +1,106 @@
 package com.kravchenko.apps.gooddeed.repository;
 
+import android.content.Context;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.kravchenko.apps.gooddeed.R;
+
 public class AuthRepository {
+
+    private final String TAG = "TAG_DEBUG_" + getClass().getSimpleName();
+    private final FirebaseAuth mAuth;
+    private final GoogleSignInClient mGoogleSignIn;
+    private final MutableLiveData<FirebaseUser> mUser;
+
+    public AuthRepository(Context context) {
+        mAuth = FirebaseAuth.getInstance();
+        mUser = new MutableLiveData<>();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(context.getString(R.string.default_web_client_id))  // OAuth 2.0 web client ID
+                .requestEmail()
+                .build();
+        mGoogleSignIn = GoogleSignIn.getClient(context, gso);
+
+        if (mAuth.getCurrentUser() != null) {
+            mUser.setValue(mAuth.getCurrentUser());
+        } else if (GoogleSignIn.getLastSignedInAccount(context) != null) {
+            firebaseAuthWithGoogle(GoogleSignIn.getLastSignedInAccount(context).getIdToken());
+        }
+    }
+
+    public void loginWithEmailAndPassword(String email, String password) {
+        isEmailRegistered(email, isRegistered -> {
+            if (isRegistered) {
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                // Login successful
+                                mUser.setValue(mAuth.getCurrentUser());
+                                Log.d(TAG, "User logged in successfully");
+                            } else {
+                                mUser.setValue(null);
+                                Log.w(TAG, "Sign in failure: " + task.getException());
+                            }
+                        });
+            } else {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "New user created successfully");
+                                mUser.setValue(mAuth.getCurrentUser());
+                            } else {
+                                mUser.setValue(null);
+                                Log.w(TAG, "Registration failure: " + task.getException());
+                            }
+                        });
+            }
+        });
+    }
+
+    public void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Sign in with credential: success");
+                        mUser.setValue(mAuth.getCurrentUser());
+                    } else {
+                        Log.w(TAG, "Sign in with credential: failure" + task.getException());
+                    }
+                });
+    }
+
+    public GoogleSignInClient loginWithGoogle() {
+        return mGoogleSignIn;
+    }
+
+    public void signOutUser() {
+        if (mAuth.getCurrentUser() != null) {
+            mAuth.signOut();
+            mGoogleSignIn.signOut();
+        }
+    }
+
+    private void isEmailRegistered(String email, OnEmailCheckListener listener) {
+        mAuth.fetchSignInMethodsForEmail(email).addOnCompleteListener(task ->
+                listener.onResult(!task.getResult().getSignInMethods().isEmpty()));
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return mUser;
+    }
+
+    private interface OnEmailCheckListener {
+        void onResult(boolean isRegistered);
+    }
 }
--- app/src/main/java/com/kravchenko/apps/gooddeed/screen/LoginFragment.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/screen/LoginFragment.java
@@ -1,16 +1,33 @@
 package com.kravchenko.apps.gooddeed.screen;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.kravchenko.apps.gooddeed.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.kravchenko.apps.gooddeed.databinding.FragmentLoginBinding;
+import com.kravchenko.apps.gooddeed.viewmodel.AuthViewModel;
+
+import static android.app.Activity.RESULT_OK;
 
 public class LoginFragment extends Fragment {
+
+    private static final int RC_SIGN_IN = 100;
+    private final String TAG = "TAG_DEBUG_" + getClass().getSimpleName();
+    private FragmentLoginBinding mBinding;
+    private AuthViewModel mAuthViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,8 +35,62 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        mBinding = FragmentLoginBinding.inflate(inflater, container, false);
+        mBinding.setFragment(this);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAuthViewModel = new ViewModelProvider(requireActivity()).get(AuthViewModel.class);
+        mAuthViewModel.getUser().observe(getViewLifecycleOwner(), firebaseUser -> {
+            if (firebaseUser != null) {
+                // Login successful
+                Toast.makeText(getContext(), "Authentication successful", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Login Successful!");
+            } else {
+                // Invalid password or email
+                Toast.makeText(getContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+    public void onLoginClick() {
+        mAuthViewModel.loginWithEmailAndPassword(mBinding.editTextLoginFragmentLogin.getText().toString().trim(),
+                mBinding.editTextLoginFragmentPassword.getText().toString().trim());
+    }
+
+    public void onLoginWithGoogleClick() {
+        startActivityForResult(mAuthViewModel.loginWithGoogle().getSignInIntent(), RC_SIGN_IN);
+    }
+
+    public void onLoginWithFacebookClick() {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "Firebase Auth with Google: " + account.getEmail());
+                mAuthViewModel.setIdToken(account.getIdToken());
+            } catch (ApiException e) {
+                // Google sign-in failed
+                Log.w(TAG, "Google sign-in failed: " + e);
+            }
+        }
     }
 }
