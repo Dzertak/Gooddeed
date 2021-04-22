9ffeadade68db88300a0420cc57218eb150234bb
Dzertak <kraftchenko.io@gmail.com>
2021-04-22 20:49:16 +0300

    added RatingBar

--- app/src/main/java/com/kravchenko/apps/gooddeed/screen/ProfileFragment.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/screen/ProfileFragment.java
@@ -2,11 +2,14 @@ package com.kravchenko.apps.gooddeed.screen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 import com.kravchenko.apps.gooddeed.R;
 
@@ -21,5 +24,12 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        RatingBar ratingBar = view.findViewById(R.id.rating_bar);
+//        ratingBar.setRating(4.3f);
     }
 }
