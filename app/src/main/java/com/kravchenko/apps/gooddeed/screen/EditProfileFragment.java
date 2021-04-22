7f2ac8e862a233e7ab2f52090098113b0418484e
dmitrysukhov96 <dmitrysukhov96@gmail.com>
2021-04-22 19:23:50 +0300

    profile fragment and more

--- app/src/main/java/com/kravchenko/apps/gooddeed/screen/EditProfileFragment.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/screen/EditProfileFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 import com.kravchenko.apps.gooddeed.R;
 
-public class AccountFragment extends Fragment {
+public class EditProfileFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,6 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
     }
 }
--- app/src/main/java/com/kravchenko/apps/gooddeed/screen/ProfileFragment.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/screen/ProfileFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 import com.kravchenko.apps.gooddeed.R;
 
-public class EditAccountFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,6 @@ public class EditAccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_account, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
