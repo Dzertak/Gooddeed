5d0d36a197d48c661c97e9a89bac2d8f42350a9e
Yevhen Yefimov <yefimovyevhen@gmail.com>
2021-04-23 11:48:26 +0300

    add user settings drawer toggle

--- app/src/main/java/com/kravchenko/apps/gooddeed/screen/MainFragment.java
+++ app/src/main/java/com/kravchenko/apps/gooddeed/screen/MainFragment.java
@@ -8,18 +8,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.kravchenko.apps.gooddeed.R;
-import com.kravchenko.apps.gooddeed.adapter.MainFilterRecyclerViewAdapter;
 import com.kravchenko.apps.gooddeed.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
@@ -46,8 +49,45 @@ public class MainFragment extends Fragment {
 
         ((AppCompatActivity) getActivity()).setSupportActionBar(binding.toolbarMainFragment);
         NavigationUI.setupWithNavController(binding.toolbarMainFragment, navController);
+        buildDrawerToggle();
+
+        //TODO
+        // set values
+        View headerLayout = binding.navView.getHeaderView(0);
+        TextView username = headerLayout.findViewById(R.id.tv_username);
+        ImageView userAvatar = headerLayout.findViewById(R.id.imv_ava);
+
     }
 
+    @SuppressLint("NonConstantResourceId")
+    private void buildDrawerToggle() {
+        DrawerLayout drawerLayout = binding.getRoot();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(),
+                drawerLayout,
+                binding.toolbarMainFragment,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        binding.navView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.profile_item:
+                    //TODO
+                    break;
+                case R.id.settings_item:
+                    //TODO
+                    break;
+                case R.id.initiative_item:
+                    //TODO
+                    break;
+                case R.id.sign_out_item:
+                    //TODO
+                    break;
+            }
+            return true;
+        });
+    }
 
 
     @Override
